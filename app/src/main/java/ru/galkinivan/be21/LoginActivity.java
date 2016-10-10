0895b4d328b1e16782eb9f07b10f8fc813f5c38a
Galkin I.S <dark1216@ya.ru>
2016-10-10 12:17:32 +0300

    lalala
    loll

--- app/src/main/java/ru/galkinivan/be21/LoginActivity.java
+++ app/src/main/java/ru/galkinivan/be21/LoginActivity.java
@@ -37,6 +37,7 @@ public class LoginActivity extends AppCompatActivity{
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+    //lalalalallala
 
 }
 
